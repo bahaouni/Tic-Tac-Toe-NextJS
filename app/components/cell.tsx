685e3c516d07784b097fc1c1fbb0b0f2d2d78cf3
baha ouni <bahaouni1@gmail.com>
2023-09-05 00:24:30 +0100

    second commit

--- app/components/cell.tsx
+++ app/components/cell.tsx
@@ -1,48 +1,50 @@
-import React from 'react';
+import { Dispatch, SetStateAction } from "react";
 
 type CellProps = {
-  go: string;
-  setGo: React.Dispatch<React.SetStateAction<string>>;
   id: number;
+  go: string;
+  setGo: Dispatch<SetStateAction<string>>;
   cells: string[];
-  setCells: React.Dispatch<React.SetStateAction<string[]>>;
+  setCells: Dispatch<SetStateAction<string[]>>;
+  cell: string;
+  winningMessage: string;
 };
 
-const Cell = ({ go, setGo, id, cells, setCells }: CellProps) => {
+const Cell = ({
+  go,
+  setGo,
+  id,
+  cells,
+  setCells,
+  cell,
+  winningMessage,
+}: CellProps) => {
   const handleClick = (e) => {
-//     if (cells[id] === '') {
-//       // If the cell is empty, set its value to 'go'
-//       cells[id] = go;
+    if (winningMessage) {
+      return;
+    }
+    const notTaken = !cells[id];
 
-//       setCells([...cells]);
-// console.log(cells)
-//       // Conditionally update the 'go' state based on 'go' value
-//       if (go === 'circle') {
-//         setGo('x');
-//       } else {
-//         setGo('circle');
-//       }
-//     } else {
-//       // If the cell is not empty, you can display a message or handle it as needed
-//       console.log('Cell already taken');
-//     }
- const taken = !!cells[id]
-if (go ==="circle") {
-    handleCellChange("circle")
+    if (notTaken) {
+      if (go === "circle") {
+        handleCellChange("circle");
+        setGo("cross");
+      } else if (go === "cross") {
+        handleCellChange("cross");
+        setGo("circle");
+      }
+    }
+  };
 
-} else if (go === "cross"){
-    handleCellChange("cross")
-}};
-const handleCellChange = (CellTochange: string) =>{
-    let copyCells= [...cells]
-    copyCells[id] = cellTochange
-    if(CellTochange==="circle")
-
-}
+  const handleCellChange = (cellToChange: string) => {
+    let copyCells = [...cells];
+    copyCells[id] = cellToChange;
+    setCells(copyCells);
+  };
 
   return (
     <div className="square" onClick={handleClick}>
-      {cells[id] === '' ? '' : <h1>{go === 'circle' ? 'O' : 'X'}</h1>}
+      <div className={cell}>{cell ? (cell === "circle" ? "O" : "X") : ""}</div>
     </div>
   );
 };
